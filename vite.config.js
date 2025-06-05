702c4b09dd17a005d2fcb2eb41a0c1c09f0c9009
juwon5272 <jackson5272@naver.com>
2025-06-05 21:13:48 +0900

    Configure for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/juwon5272.github.io/", // GitHub 저장소 이름으로 수정
+  base: "/juwon5272.github.io/grid/", // GitHub 저장소 이름으로 수정
 });
 
 // import { defineConfig } from 'vite'
